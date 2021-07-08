c47b53767c4fa01fdb3450d21374e05363e11bb8
Bosco971020 <86942448+Bosco971020@users.noreply.github.com>
2021-07-08 16:26:37 +0800

    Add files via upload

--- 交叉的奇數星星塔.py
+++ 交叉的奇數星星塔.py
@@ -1,12 +1,17 @@
-x=int(input('請問你要幾層的交叉星星塔?'))
-
-
-for i in range(1,x-1,2):
-    print(' '*i+'*' + ' ' * (x*2 - i*2-1) + '*')
-for i in [x]:
-    print(' '*i+'*')
-for i in range(1,x,2):
-    print(' '*(x-i-1)+'*' + ' ' * (x*2 - (x-i)*2+1) + '*')
+x=int(input('請問你要幾層的交叉星星塔?"只能輸入奇數"'))
+if x%2==0:
+    print('奇數only!')
+elif x==1:
+    print('你輸入的數字太小了!')
+elif x>=55:
+    print('你輸入的數字太大了!')
+else:
+    for i in range(1,x-1,2):
+        print(' '*i+'*' + ' ' * (x*2 - i*2-1) + '*')
+    for i in [x]:
+        print(' '*i+'*')
+    for i in range(1,x,2):
+        print(' '*(x-i-1)+'*' + ' ' * (x*2 - (x-i)*2+1) + '*')
     
 
 
